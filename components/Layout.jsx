b981613077a5c45c54c61c4f71e09f364c834efc
Bradley Schoeneweis <bradley.schoeneweis@gmail.com>
2022-01-11 20:41:23 -0600

    Update meta image

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -8,7 +8,7 @@ export const siteTitle = 'Bradley Schoeneweis';
 const name = 'Bradley Schoeneweis';
 const metaImage = (`https://og-image.vercel.app/${encodeURI(
   siteTitle
-)}.png?theme=light&md=0&fontSize=75px&images=https%3A%2F%2Fassets.vercel.com%2Fimage%2Fupload%2Ffront%2Fassets%2Fdesign%2Fnextjs-black-logo.svg`);
+)}.png?theme=light&md=1&fontSize=50px&images=https%3A%2F%2Fassets.vercel.com%2Fimage%2Fupload%2Ffront%2Fassets%2Fdesign%2Fhyper-color-logo.svg&widths=350&heights=350`);
 
 export default function Layout({ children, home, tagPage }) {
   let tagLink;
