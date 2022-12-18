4a00212541fc8d15b26a56cadfbd05e3e78433aa
Bradley Schoeneweis <bradley.schoeneweis@gmail.com>
2022-12-18 15:37:09 -0600

    Remove other links

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -8,33 +8,26 @@ const Footer = () => {
   return (
     <footer className={styles.FooterContainer}>
       <div className={styles.Footer}>
-        <div className={styles.LeftFooterWrapper}>
-          <span className={styles.SocialLinkWrapper}>
-            <a href="https://www.linkedin.com/in/bradley-schoeneweis/" target="_blank" rel="noreferrer">
-              <img width="16" src="/icons/linkedin.svg" alt="linkedin logo" className={styles.SocialLinkLogo}></img>
-            </a>
-            <a href="https://github.com/bschoeneweis" target="_blank" rel="noreferrer">
-              <img width="16" src="/icons/github.svg" alt="github logo" className={styles.SocialLinkLogo}></img>
-            </a>
-            <a href="https://medium.com/@bradley-schoeneweis" target="_blank" rel="noreferrer">
-              <img width="16" src="/icons/medium.svg" alt="medium logo" className={styles.SocialLinkLogo}></img>
-            </a>
-            <a href="https://dev.to/bschoeneweis" target="_blank" rel="noreferrer">
-              <img width="16" src="/icons/dev.svg" alt="dev logo" className={styles.SocialLinkLogo}></img>
-            </a>
-            <a href="/feed.xml">
-              <img width="16" src="/icons/rss.svg" alt="rss logo" className={styles.SocialLinkLogo}></img>
-            </a>
-          </span>
+        <div className={styles.SocialLinkWrapper}>
+          <a href="https://www.linkedin.com/in/bradley-schoeneweis/" target="_blank" rel="noreferrer">
+            <img width="16" src="/icons/linkedin.svg" alt="linkedin logo" className={styles.SocialLinkLogo}></img>
+          </a>
+          <a href="https://github.com/bschoeneweis" target="_blank" rel="noreferrer">
+            <img width="16" src="/icons/github.svg" alt="github logo" className={styles.SocialLinkLogo}></img>
+          </a>
+          <a href="https://medium.com/@bradley-schoeneweis" target="_blank" rel="noreferrer">
+            <img width="16" src="/icons/medium.svg" alt="medium logo" className={styles.SocialLinkLogo}></img>
+          </a>
+          <a href="https://dev.to/bschoeneweis" target="_blank" rel="noreferrer">
+            <img width="16" src="/icons/dev.svg" alt="dev logo" className={styles.SocialLinkLogo}></img>
+          </a>
+          <a href="/feed.xml">
+            <img width="16" src="/icons/rss.svg" alt="rss logo" className={styles.SocialLinkLogo}></img>
+          </a>
         </div>
-        <div className={styles.SiteLinks}>
-          <Link href="/books">
-            <a>📚</a>
-          </Link>
+        <div className={styles.CopyrightWrapper}>
+          <small>© <time>{YEAR}</time> Bradley Schoeneweis</small>
         </div>
-      </div>
-      <div className={styles.CopyrightWrapper}>
-        <small>© <time>{YEAR}</time> Bradley Schoeneweis</small>
       </div>
     </footer>
   );
